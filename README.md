3c187e829bead73cd4ebce2f1bf91ee63ca609f2
Stephan Pitois <spitois@gmail.com>
2021-03-21 20:21:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Amelia Musical Playhouse Static API
+
+```
+python3 build.py
+```
+
+## Environment Variables
+
+- AMP_STATIC_API_GRAPHQL_URI
---
+++ build.py
@@ -0,0 +1,26 @@
+import json
+import os
+import requests
+from requests.exceptions import HTTPError
+from decouple import config
+
+url = config('AMP_STATIC_API_GRAPHQL_URI')
+data = {'query': '{ productions(first: 20, where: {categoryNotIn: 21}) { edges { node { id title productionDetails { ampEventOneParagraphSummary } productionGallery { ampEventPhotoCredit ampEventPhotos { title uri } } productionDates { ampEventStartDate ampEventDatesTimes { ampEventDateTime } } productionTickets { ampEventExternalTicketing ampEventGetTicketsUrl ampEventTicketPrices { ampEventAdmissionLevel ampEventPrice } } productionPeople { fieldGroupName ampEventFeaturing ampEventFullBand ampEventFullCast ampEventStarring ampProductionTeam ampEventPersonnel { ampEventPersonnelName ampEventPersonnelRole } ampEventCrew { ampEventCrewName ampEventCrewRole } } featuredImage { node { sourceUrl(size: MEDIUM_LARGE) } } link } } } }'}
+
+base_path = 'data/'
+os.makedirs(base_path, exist_ok=True)
+
+try:
+    response = requests.post(url, data = data)
+    response.raise_for_status()
+    jsonResponse = response.json()
+    filename = base_path + 'upcoming-productions.json'
+    with open(filename, 'w') as f:
+        json.dump(jsonResponse, f, ensure_ascii=False)
+
+except HTTPError as http_err:
+    print('HTTP error occurred:')
+    print(http_err)
+except Exception as err:
+    print('Error occurred:')
+    print(err)
