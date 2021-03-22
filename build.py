e65a12be15d61612e53c897af7ba61e744cfc22c
Stephan Pitois <spitois@gmail.com>
2021-03-21 23:01:30 -0400

    More logs

--- build.py
+++ build.py
@@ -5,6 +5,8 @@ from requests.exceptions import HTTPError
 from decouple import config
 
 url = config('AMP_STATIC_API_GRAPHQL_URI')
+print('url: ' + url)
+
 data = {'query': '{ productions(first: 20, where: {categoryNotIn: 21}) { edges { node { id title productionDetails { ampEventOneParagraphSummary } productionGallery { ampEventPhotoCredit ampEventPhotos { title uri } } productionDates { ampEventStartDate ampEventDatesTimes { ampEventDateTime } } productionTickets { ampEventExternalTicketing ampEventGetTicketsUrl ampEventTicketPrices { ampEventAdmissionLevel ampEventPrice } } productionPeople { fieldGroupName ampEventFeaturing ampEventFullBand ampEventFullCast ampEventStarring ampProductionTeam ampEventPersonnel { ampEventPersonnelName ampEventPersonnelRole } ampEventCrew { ampEventCrewName ampEventCrewRole } } featuredImage { node { sourceUrl(size: MEDIUM_LARGE) } } link } } } }'}
 
 base_path = 'data/'
@@ -12,6 +14,8 @@ os.makedirs(base_path, exist_ok=True)
 
 try:
     response = requests.post(url, data = data)
+    print('status_code: ' + str(response.status_code))
+
     response.raise_for_status()
     jsonResponse = response.json()
     filename = base_path + 'upcoming-productions.json'
@@ -19,8 +23,8 @@ try:
         json.dump(jsonResponse, f, ensure_ascii=False)
 
 except HTTPError as http_err:
-    print('HTTP error occurred:')
-    print(http_err)
+    print('HTTP error occurred: ' +  str(http_err))
+    raise
 except Exception as err:
-    print('Error occurred:')
-    print(err)
+    print('Error occurred: ' + str(err))
+    raise
